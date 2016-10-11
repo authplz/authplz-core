4139ac0e68fe327df8dd1332e534d96e919eb1be
Ryan Kurte <ryankurte@gmail.com>
2016-10-11 15:10:16 +1300

    Fixed session middleware

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ import "github.com/asaskevich/govalidator"
 import "github.com/ryankurte/authplz/usercontroller"
 import "github.com/ryankurte/authplz/datastore"
 
+// Application context
+// TODO: this could be split and bound by module
 type AuthPlzCtx struct {
 	port           string
 	address        string
@@ -25,7 +27,9 @@ type AuthPlzCtx struct {
 	sessionStore   *sessions.CookieStore
 }
 
+// User session layer
 func (ctx *AuthPlzCtx) SessionMiddleware(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
+	fmt.Println(ctx)
 	session, err := ctx.sessionStore.Get(req.Request, "user-session")
 	if err != nil {
 		fmt.Println(err);
@@ -37,6 +41,23 @@ func (ctx *AuthPlzCtx) SessionMiddleware(rw web.ResponseWriter, req *web.Request
 	next(rw, req)
 }
 
+// Convenience type to describe middleware functions
+type MiddlewareFunc func (ctx *AuthPlzCtx, rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc)
+
+// Bind global context into the router
+func BindContext(extCtx AuthPlzCtx) MiddlewareFunc {
+	return func (ctx *AuthPlzCtx, rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
+		// Bind global components to context
+		ctx.port 	= extCtx.port
+		ctx.address = extCtx.address
+		ctx.userController = extCtx.userController
+		ctx.sessionStore = extCtx.sessionStore
+
+		next(rw, req)
+	}
+}
+
+// Create a user
 func (c *AuthPlzCtx) Create(rw web.ResponseWriter, req *web.Request) {
 	email := req.FormValue("email")
 	if !govalidator.IsEmail(email) {
@@ -52,6 +73,7 @@ func (c *AuthPlzCtx) Create(rw web.ResponseWriter, req *web.Request) {
 	rw.WriteHeader(501)
 }
 
+// Login to a user account
 func (c *AuthPlzCtx) Login(rw web.ResponseWriter, req *web.Request) {
 	email := req.FormValue("email")
 	if !govalidator.IsEmail(email) {
@@ -67,13 +89,16 @@ func (c *AuthPlzCtx) Login(rw web.ResponseWriter, req *web.Request) {
 	rw.WriteHeader(501)
 }
 
+// Logout of a user account
 func (c *AuthPlzCtx) Logout(rw web.ResponseWriter, req *web.Request) {
 	rw.WriteHeader(501)
 }
 
+// Get user login status
 func (c *AuthPlzCtx) Status(rw web.ResponseWriter, req *web.Request) {
 	rw.WriteHeader(501)
 }
+
 
 func main() {
 	var port string = "9000"
@@ -95,8 +120,12 @@ func main() {
 	// Create controllers
 	uc := usercontroller.NewUserController(&ds, nil)
 
+	// Create a global context object
+	globalContext := AuthPlzCtx{port, address, &uc, sessionStore};
+
 	// Create router
-	router := web.New(AuthPlzCtx{port, address, &uc, sessionStore}).
+	router := web.New(AuthPlzCtx{}).
+		Middleware(BindContext(globalContext)).
 		Middleware(web.LoggerMiddleware).
 		Middleware(web.ShowErrorsMiddleware).
 		Middleware((*AuthPlzCtx).SessionMiddleware).
