5f9b8f1085f5d35ed345c6f1630cbeda53a4773f
Ryan Kurte <ryankurte@gmail.com>
2017-03-31 23:58:00 +1300

    Adding routes to config

--- lib/config/routes.go
+++ lib/config/routes.go
@@ -6,6 +6,17 @@ type Routes struct {
 	Login  string // Login is the user login page
 	Logout string // Logout is the user logout page
 
+	AccountManage string
+
+	U2FRegister  string
+	U2FManage    string
+	U2FAuthorize string
+
+	TOTPRegister  string
+	TOTPManage    string
+	TOTPAuthorize string
+
+	OAuthManage    string
 	OauthAuthorize string // OauthAuthorize page allows users to accept or deny OAuth grants
 }
 
@@ -15,6 +26,17 @@ func DefaultRoutes() Routes {
 		Login:  "/login",
 		Logout: "/logout",
 
+		AccountManage: "/account",
+
+		U2FRegister:  "/u2f/register",
+		U2FManage:    "/u2f/manage",
+		U2FAuthorize: "/u2f/authorize",
+
+		TOTPRegister:  "/totp/register",
+		TOTPManage:    "/totp/manage",
+		TOTPAuthorize: "/totp/authorize",
+
+		OAuthManage:    "/oauth/manage",
 		OauthAuthorize: "/oauth/authorize",
 	}
 }
