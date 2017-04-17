8556eba332e56f83fc03387e09d35f039b469733
Ryan Kurte <ryankurte@gmail.com>
2017-04-18 10:49:38 +1200

    Oh no

---
+++ cmd/authplz/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/ryankurte/authplz/lib/app"
+	"github.com/ryankurte/authplz/lib/config"
+)
+
+func main() {
+
+	// Load configuration
+	c := config.GetConfig()
+
+	// Create server instance
+	server := app.NewServer(*c)
+
+	// Launch server
+	server.Start()
+}
